8d296dd620d3954d776a09b53427cba9011baa73
aforce-Yamada <k.yamada@a-force.co.jp>
2020-07-02 19:09:48 +0900

    refs #6 1-4課題修正

--- python1_test4.py
+++ python1_test4.py
@@ -1,16 +1,16 @@
 debt = 250000
 interest = (0.14 / 12)
 repayment = 30000
-n = 0
+month = 0
 
 while debt > 0:
-    n += 1
+    month += 1
     debt = debt + (debt * interest)
 
     if debt > repayment:
         debt -= repayment
-        print(str(n) + "ヶ月目：返済額=" + str(repayment) + "円, 残り" + str(debt) + "円")
+        print(str(month) + "ヶ月目：返済額=" + str(repayment) + "円, 残り" + str(debt) + "円")
     else:
         repayment = debt
         debt = 0
-        print(str(n) + "ヶ月目：返済額=" + str(repayment) + "円, 返済完了")
+        print(str(month) + "ヶ月目：返済額=" + str(repayment) + "円, 返済完了")
