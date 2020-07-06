cc00d57cd75eb1977e8e34170820fe9d28f8ea0d
aforce-Yamada <k.yamada@a-force.co.jp>
2020-07-06 13:34:25 +0900

    refs #5 3-6 課題提出

---
+++ python3_test6.py
@@ -0,0 +1,11 @@
+import python3_test5
+SmartPhone = python3_test5.SmartPhone
+
+SmartPhone.play_music()
+SmartPhone.next_song()
+SmartPhone.previous_song()
+SmartPhone.stop_music()
+
+taro = SmartPhone("太郎" , "taro@maill.com")
+taro.call()
+taro.send_mail()
