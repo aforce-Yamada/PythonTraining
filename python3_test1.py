ac899768c4fe7d0afdcd86c80b66e5a0ea6e41e2
aforce-Yamada <k.yamada@a-force.co.jp>
2020-07-06 13:31:23 +0900

    refs #5 3-1 課題提出

---
+++ python3_test1.py
@@ -0,0 +1,13 @@
+class MP3Player:
+
+    def play_music():
+        print("音楽を再生します。")
+
+    def next_song():
+        print("次の曲を再生します。")
+
+    def previous_song():
+        print("前の曲を再生します。")
+    
+    def stop_music():
+        print("音楽を止めます。")
