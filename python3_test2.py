094ad49719c03c5cdd226349ce776eff808c3af5
aforce-Yamada <k.yamada@a-force.co.jp>
2020-07-07 11:03:44 +0900

    refs #5 3-2 課題修正

--- python3_test2.py
+++ python3_test2.py
@@ -1,7 +1,7 @@
-import python3_test1
-MP3Player = python3_test1.MP3Player #インスタンス化
+from python3_test1 import MP3Player
+mp3player = MP3Player() #インスタンス化
 
-MP3Player.play_music()
-MP3Player.next_song()
-MP3Player.previous_song()
-MP3Player.stop_music()
+mp3player.play_music()
+mp3player.next_song()
+mp3player.previous_song()
+mp3player.stop_music()
