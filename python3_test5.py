fb8e4fae483d53b60de1ec94a23fd86a7a24a52b
aforce-Yamada <k.yamada@a-force.co.jp>
2020-07-07 11:04:14 +0900

    refs #5 3-5 課題修正

--- python3_test5.py
+++ python3_test5.py
@@ -1,7 +1,5 @@
-import python3_test1
-import python3_test3
-MP3Player = python3_test1.MP3Player
-CellPhone = python3_test3.CellPhone
+from python3_test1 import MP3Player
+from python3_test3 import CellPhone
 
 class SmartPhone(MP3Player, CellPhone):
     pass
